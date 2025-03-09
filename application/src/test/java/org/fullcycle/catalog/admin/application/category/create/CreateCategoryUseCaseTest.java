ee4762b41892cf7517d947ed15ba0ed6de98b7f8
João Paulo Ferreira <paulojp.dev@gmail.com>
2025-03-09 16:15:57 -0300

    refactor use case test

--- application/src/test/java/org/fullcycle/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/org/fullcycle/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,42 +1,64 @@
 package org.fullcycle.catalog.admin.application.category.create;
 
 import org.fullcycle.catalog.admin.domain.category.CategoryGateway;
+import org.fullcycle.catalog.admin.domain.exception.DomainValidationException;
+import org.fullcycle.catalog.admin.domain.validation.Message;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.AdditionalAnswers;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.times;
-
 @ExtendWith(MockitoExtension.class)
 public class CreateCategoryUseCaseTest {
 
+    @InjectMocks
+    private CreateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
     @Test
     public void givenAValidCommand_whenCreateACategory_thenReturnAValidCategory() {
-        final var name = "Category";
-        final var description = "Description";
-        final var isActive = true;
-        final var command = CreateCategoryCommand.of(name, description, isActive);
-        final var categoryGateway = Mockito.mock(CategoryGateway.class);
+        final var expectedName = "Category";
+        final var expectedDescription = "Description";
+        final var expectedIsActive = true;
+        final var command = CreateCategoryCommand.of(expectedName, expectedDescription, expectedIsActive);
+
         Mockito.when(categoryGateway.create(Mockito.any()))
                 .thenAnswer(AdditionalAnswers.returnsFirstArg());
-        final var useCase = new CreateCategoryUseCase(categoryGateway);
+
         final var output = useCase.execute(command);
+
         Assertions.assertNotNull(output);
         Assertions.assertNotNull(output.id());
-        Mockito.verify(categoryGateway, times(1)).create(argThat(
-                category -> Objects.equals(name, category.getName())
-                        && Objects.equals(description, category.getDescription())
-                        && Objects.equals(isActive, category.isActive())
-                        && Objects.nonNull(category.getId())
-                        && Objects.nonNull(category.getCreatedAt())
-                        && Objects.nonNull(category.getUpdatedAt())
-                        && Objects.equals(true, category.getDeletedAt().isEmpty())
-        ));
+        Mockito.verify(categoryGateway, Mockito.times(1))
+                .create(ArgumentMatchers.argThat(
+                        category -> Objects.equals(expectedName, category.getName())
+                                && Objects.equals(expectedDescription, category.getDescription())
+                                && Objects.equals(expectedIsActive, category.isActive())
+                                && Objects.nonNull(category.getId())
+                                && Objects.nonNull(category.getCreatedAt())
+                                && Objects.nonNull(category.getUpdatedAt())
+                                && Objects.equals(category.getCreatedAt(), category.getUpdatedAt())
+                                && Objects.equals(true, category.getDeletedAt().isEmpty())
+                ));
+    }
+
+    @Test
+    public void givenAInvalidCommand_whenCreateACategory_thenThrowException() {
+        final var expectedMessage = Message.resolve("name", Message.NOT_NULL);
+        final var command = CreateCategoryCommand.of(null, null, true);
+        final var expectedErrorCount = 1;
+
+        Executable executable = () -> useCase.execute(command);
+        final var exception = Assertions.assertThrows(DomainValidationException.class, executable);
+
+        Assertions.assertEquals(expectedMessage, exception.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, exception.getErrors().size());
+        Mockito.verify(categoryGateway, Mockito.never()).create(ArgumentMatchers.any());
     }
 }
