d849ef4d427a16df68a73246e7030672cdd647ca
João Paulo Ferreira <paulojp.dev@gmail.com>
2025-03-22 20:40:20 -0300

    require page and quantityPerPage for SearchQuery

--- domain/src/main/java/org/fullcycle/catalog/admin/domain/pagination/SearchQuery.java
+++ domain/src/main/java/org/fullcycle/catalog/admin/domain/pagination/SearchQuery.java
@@ -7,4 +7,12 @@ public record SearchQuery(
         String sortField,
         String sortDirection
 ) {
+    public SearchQuery {
+        if (page == null || page < 0) {
+            throw new IllegalArgumentException("Page must be a non-null, non-negative integer");
+        }
+        if (quantityPerPage == null || quantityPerPage <= 0) {
+            throw new IllegalArgumentException("Quantity per page must be a non-null, positive integer");
+        }
+    }
 }
---
+++ domain/src/test/java/org/fullcycle/catalog/admin/domain/pagination/SearchQueryTest.java
@@ -0,0 +1,63 @@
+package org.fullcycle.catalog.admin.domain.pagination;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class SearchQueryTest {
+
+    static Stream<Arguments> invalidCategoryData() {
+        final var pageError = "Page must be a non-null, non-negative integer";
+        final var quantityError = "Quantity per page must be a non-null, positive integer";
+        return Stream.of(
+            Arguments.of(null, 10, "terms", "name", "asc", pageError),
+            Arguments.of(-1, 10, "terms", "name", "asc", pageError),
+            Arguments.of(10, null, "terms", "name", "asc", quantityError),
+            Arguments.of(10, 0, "terms", "name", "asc", quantityError),
+            Arguments.of(10, -1, "terms", "name", "asc", quantityError));
+    }
+
+    static Stream<Arguments> validCategoryData() {
+        return Stream.of(
+            Arguments.of(0, 1, "terms", "name", "asc"),
+            Arguments.of(10, 100, null, null, null),
+            Arguments.of(1, 2, null, "field", null));
+    }
+
+    @ParameterizedTest
+    @MethodSource("validCategoryData")
+    public void givenAllParams_whenInstantiate_thenValidQuery(Integer page,
+                                                              Integer quantityPerPage,
+                                                              String terms,
+                                                              String sortField,
+                                                              String sortDirection) {
+        final var actualQuery = new SearchQuery(
+            page,
+            quantityPerPage,
+            terms,
+            sortField,
+            sortDirection);
+        Assertions.assertEquals(page, actualQuery.page());
+        Assertions.assertEquals(quantityPerPage, actualQuery.quantityPerPage());
+        Assertions.assertEquals(terms, actualQuery.whereFilterTerms());
+        Assertions.assertEquals(sortField, actualQuery.sortField());
+        Assertions.assertEquals(sortDirection, actualQuery.sortDirection());
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidCategoryData")
+    public void givenNullPageParam_whenInstantiate_thenValidQuery(Integer page,
+                                                                  Integer quantityPerPage,
+                                                                  String terms,
+                                                                  String sortField,
+                                                                  String sortDirection,
+                                                                  String errorMessage) {
+        Executable executable = () -> new SearchQuery(page, quantityPerPage, terms, sortField, sortDirection);
+        final var exception = Assertions.assertThrows(IllegalArgumentException.class, executable);
+        Assertions.assertEquals(errorMessage, exception.getMessage());
+    }
+}
