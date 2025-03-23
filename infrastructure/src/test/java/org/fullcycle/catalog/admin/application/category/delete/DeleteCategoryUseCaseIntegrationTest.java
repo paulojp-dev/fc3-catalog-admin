86123eebb17cec6725b4e9c079a85e9e2bda6af6
João Paulo Ferreira <paulojp.dev@gmail.com>
2025-03-23 11:40:59 -0300

    delete category use case integration test

---
+++ infrastructure/src/test/java/org/fullcycle/catalog/admin/application/category/delete/DeleteCategoryUseCaseIntegrationTest.java
@@ -0,0 +1,48 @@
+package org.fullcycle.catalog.admin.application.category.delete;
+
+import org.fullcycle.catalog.admin.IntegrationTest;
+import org.fullcycle.catalog.admin.domain.base.ID;
+import org.fullcycle.catalog.admin.domain.category.Category;
+import org.fullcycle.catalog.admin.domain.category.CategoryGateway;
+import org.fullcycle.catalog.admin.infrastructure.category.persistence.CategoryJpaEntity;
+import org.fullcycle.catalog.admin.infrastructure.category.persistence.CategoryJpaRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@IntegrationTest
+public class DeleteCategoryUseCaseIntegrationTest {
+
+    @Autowired
+    private DeleteCategoryUseCase useCase;
+
+    @Autowired
+    private CategoryJpaRepository repository;
+
+    @Spy
+    private CategoryGateway gateway;
+
+    @Test
+    public void givenAnIdOfExistingCategory_whenExecute_thenDeleteCategory() {
+        final var existingCategory = Category.of("Name", "Description", true);
+        final var expectedId = existingCategory.getId();
+        repository.saveAndFlush(CategoryJpaEntity.from(existingCategory));
+        Assertions.assertEquals(1, repository.count());
+        useCase.execute(expectedId.getValue());
+        Assertions.assertEquals(0, repository.count());
+    }
+
+    @Test
+    public void givenAnIdOfNonExistingCategory_whenExecute_thenDoNothing() {
+        final var id = ID.of(java.util.UUID.randomUUID());
+        Assertions.assertEquals(0, repository.count());
+        useCase.execute(id.getValue());
+        Assertions.assertEquals(0, repository.count());
+        Mockito.verify(gateway, Mockito.never()).deleteById(Mockito.any());
+    }
+}
