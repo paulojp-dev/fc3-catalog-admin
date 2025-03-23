24ef5a70604e545591f01a6b9df7f378d75c89aa
João Paulo Ferreira <paulojp.dev@gmail.com>
2025-03-23 11:32:35 -0300

    create category use case integration test

---
+++ infrastructure/src/test/java/org/fullcycle/catalog/admin/IntegrationTest.java
@@ -0,0 +1,22 @@
+package org.fullcycle.catalog.admin;
+
+import jakarta.transaction.Transactional;
+import org.fullcycle.catalog.admin.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test")
+@SpringBootTest(classes = WebServerConfig.class)
+@Transactional
+public @interface IntegrationTest {
+
+}
---
+++ infrastructure/src/test/java/org/fullcycle/catalog/admin/application/category/create/CreateCategoryUseCaseIntegrationTest.java
@@ -0,0 +1,61 @@
+package org.fullcycle.catalog.admin.application.category.create;
+
+import org.fullcycle.catalog.admin.IntegrationTest;
+import org.fullcycle.catalog.admin.domain.category.CategoryGateway;
+import org.fullcycle.catalog.admin.domain.exception.DomainValidationException;
+import org.fullcycle.catalog.admin.domain.validation.Message;
+import org.fullcycle.catalog.admin.infrastructure.category.persistence.CategoryJpaRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SuppressWarnings({
+    "SpringJavaInjectionPointsAutowiringInspection",
+    "OptionalGetWithoutIsPresent"
+})
+@IntegrationTest
+public class CreateCategoryUseCaseIntegrationTest {
+
+    @Autowired
+    private CreateCategoryUseCase useCase;
+
+    @Autowired
+    private CategoryJpaRepository repository;
+
+    @Spy
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidCommand_whenExecute_thenReturnTheCreatedCategory() {
+        final var expectedName = "Category Name";
+        final var expectedDescription = "Category Description";
+        final var expectedIsActive = true;
+        Assertions.assertEquals(0, repository.count());
+        final var command = CreateCategoryCommand.of(expectedName, expectedDescription, expectedIsActive);
+        final var result = useCase.execute(command);
+        final var actualCategory = repository.findById(result.id().getValue()).get();
+        Assertions.assertEquals(1, repository.count());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.getActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void givenAInvalidCommand_whenCreateACategory_thenThrowException() {
+        final var expectedMessage = Message.resolve("name", Message.NOT_NULL);
+        final var command = CreateCategoryCommand.of(null, null, true);
+        final var expectedErrorCount = 1;
+        Executable executable = () -> useCase.execute(command);
+        final var exception = Assertions.assertThrows(DomainValidationException.class, executable);
+        Assertions.assertEquals(expectedMessage, exception.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, exception.getErrors().size());
+        Mockito.verify(categoryGateway, Mockito.never()).create(ArgumentMatchers.any());
+    }
+}
